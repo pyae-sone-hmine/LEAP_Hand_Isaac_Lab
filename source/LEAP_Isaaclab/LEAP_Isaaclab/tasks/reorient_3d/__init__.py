68746302847ad38f42d4d09caa1d1f0e7b9d2dc3
mfpeticco <mfpeticco@yahoo.com>
2025-12-02 23:44:20 -0500

    fixes

--- source/LEAP_Isaaclab/LEAP_Isaaclab/tasks/reorient_3d/__init__.py
+++ source/LEAP_Isaaclab/LEAP_Isaaclab/tasks/reorient_3d/__init__.py
@@ -14,10 +14,10 @@ reorient_task_entry = "LEAP_Isaaclab.tasks.reorient_3d"
 
 gym.register(
     id="Reorient_Cube_3D",
-    entry_point=f"{reorient_task_entry}.reorientation_env_3d:ReorientationEnv3d",
+    entry_point=f"{reorient_task_entry}.reorientation_env_3d:ReorientationEnv3D",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.leap_hand_env_cfg_3d:LeapHandEnvCfg3d",
+        "env_cfg_entry_point": f"{__name__}.leap_hand_env_cfg_3d:LeapHandEnvCfg3D",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LeapHandPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
